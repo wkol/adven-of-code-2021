0d8b17c3008659b914e9ee0f11b920b5a562bbf0
wkolodz2 <01158744@pw.edu.pl>
2021-12-02 13:44:31 +0100

    Solve Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        for (i in 0..input.size - 2) {
+            if(input[i] < input[i + 1]) {
+                counter++
+            }
+        }
+        return counter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        for (i in 0..input.size - 4) {
+            if (input.subList(i, i+3).sumOf { it.toInt() } < input.subList(i+1, i+4).sumOf { it.toInt() })
+                counter++
+        }
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+
+    val testInput2 = readInput("Day01_test")
+    check(part2(testInput2) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
